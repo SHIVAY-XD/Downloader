081fee3d234a441c1ff0c46dbc7bac6b8c5697db
S ʜ ɪ ᴠ ᴀ ʏ <115808731+SHIVAY-XD@users.noreply.github.com>
2024-10-26 12:55:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters  # Add CommandHandler and MessageHandler
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from handlers.start import start
 from handlers.info import info
 from handlers.download import handle_message
 from handlers.broadcast import broadcast
 
 TELEGRAM_BOT_TOKEN = '6996568724:AAFrjf88-0uUXJumDiuV6CbVuXCJvT-4KbY'
+
+# Create a ThreadPoolExecutor
+executor = ThreadPoolExecutor(max_workers=4)  # Adjust max_workers as needed
 
 def main():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
@@ -15,6 +20,10 @@ def main():
     application.add_handler(CommandHandler("broadcast", broadcast))
 
     application.run_polling()
+
+async def run_in_executor(func, *args):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(executor, func, *args)
 
 if __name__ == '__main__':
     main()
