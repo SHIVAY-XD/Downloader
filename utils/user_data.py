300b901084af2a401fc3e3dbbdf7843651b0ffcc
S ʜ ɪ ᴠ ᴀ ʏ <115808731+SHIVAY-XD@users.noreply.github.com>
2024-10-26 12:41:25 +0530

    Update user_data.py

--- utils/user_data.py
+++ utils/user_data.py
@@ -1,1 +1,23 @@
+import json
+
+USER_DATA_FILE = "user_details.json"
+
+# Load user details from a JSON file
+try:
+    with open(USER_DATA_FILE, "r") as file:
+        user_details = json.load(file)
+except FileNotFoundError:
+    user_details = []
+
+def save_user_details():
+    with open(USER_DATA_FILE, "w") as file:
+        json.dump(user_details, file, indent=4)
+
+async def check_channel_membership(user_id: int, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        chat_member = await context.bot.get_chat_member('@itsteachteam', user_id)
+        return chat_member.status in ["member", "administrator", "creator"]
+    except Exception as e:
+        print(f"Error checking channel membership: {e}")
+        return False
 
