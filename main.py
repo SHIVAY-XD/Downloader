23f8b1cc65f355a28d7be5d886d7668546dd47f7
S ʜ ɪ ᴠ ᴀ ʏ <115808731+SHIVAY-XD@users.noreply.github.com>
2024-10-26 13:15:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-from handlers.start import start  # Importing directly
-from handlers.info import info  # Importing directly
-from handlers.download import download_and_send_video  # Importing directly
-from handlers.broadcast import broadcast  # Importing directly
+from handlers.start import start
+from handlers.info import info
+from handlers.download import download_and_send_video
+from handlers.broadcast import broadcast
 
-TELEGRAM_BOT_TOKEN = '6996568724:AAFrjf88-0uUXJumDiuV6CbVuXCJvT-4KbY'
+TELEGRAM_BOT_TOKEN = '6996568724:AAFrjf88-0uUXJumDiuV6CbVuXCJvT-4KbY'  # Replace with your actual token
 
-# Create a ThreadPoolExecutor
-executor = ThreadPoolExecutor(max_workers=4)  # Adjust as needed
-
-async def run_in_executor(func, *args):
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(executor, func, *args)
+# Semaphore to limit concurrent downloads
+max_concurrent_downloads = 5
+semaphore = asyncio.Semaphore(max_concurrent_downloads)
 
 async def handle_download(update, context):
     video_url = update.message.text
     user_id = update.effective_user.id
     chat_id = update.message.chat_id
 
-    await download_and_send_video(video_url, chat_id, user_id, context)
+    async with semaphore:
+        await download_and_send_video(video_url, chat_id, user_id, context)
 
 def main():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("info", info))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_download))  # Using the new handler
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_download))
     application.add_handler(CommandHandler("broadcast", broadcast))
 
     application.run_polling()
